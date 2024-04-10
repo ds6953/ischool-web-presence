f4f9a517438da151659ef5729eabaa98a5cdb212
Drew Silberman <drewsilberman@drewsmac.student.rit.edu>
2024-04-10 16:01:36 -0400

    commiting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import About from './components/about';
+import Degrees from './components/Degrees';
+import Minors from './components/Minors';
+import Employment from './components/Employment';
+import People from './components/People';
+import CoursesButton from './components/CoursesButton';
+import './App.css'; // Assuming you have a CSS file for basic styling
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <header className="app-header">
+          {/* Navigation Links */}
+          <nav>
+            <ul className="nav-links">
+              <li><Link to="/">About</Link></li>
+              <li><Link to="/degrees">Degrees</Link></li>
+              <li><Link to="/minors">Minors</Link></li>
+              <li><Link to="/employment">Employment</Link></li>
+              <li><Link to="/people">People</Link></li>
+              <li><Link to="/courses">Courses</Link></li>
+            </ul>
+          </nav>
+        </header>
+        
+        {/* Route Configuration */}
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/degrees" element={<Degrees />} />
+          <Route path="/minors" element={<Minors />} />
+          <Route path="/employment" element={<Employment />} />
+          <Route path="/people" element={<People />} />
+          <Route path="/courses" element={<CoursesButton />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
